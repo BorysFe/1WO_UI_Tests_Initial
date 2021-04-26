c7db3522dd1565a691f2884525c1f78a1d4cc066
BorysFe <fesenko@icloud.com>
2021-04-26 21:21:42 +0300

    Activities Expected conditions Wait utils

---
+++ 1WO_UI_Tests-master/src/main/java/utils/WOActivities.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class WOActivities extends DriverFactory {
+
+    public WOActivities(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+}
--- 1WO_UI_Tests-master/src/main/java/utils/WaitUtils.java
+++ 1WO_UI_Tests-master/src/main/java/utils/WaitUtils.java
@@ -1,10 +1,12 @@
 package utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.conditions.WOExpectedConditions;
 
 import java.util.List;
 
@@ -13,11 +15,23 @@ public class WaitUtils extends DriverFactory {
     private final int shortTimeout = 5;
     private final int longTimeout = 15;
 
+    public static final int SHORT_TIMEOUT = 5;
+    private WebDriverWait shortWait;
+
+    public static final int MIDDLE_TIMEOUT = 10;
+    private WebDriverWait midWait;
+
+    public static final int LONG_TIMEOUT = 15;
+    private WebDriverWait longWait;
+
+    public static final int EMAIL_ENGAGEMENT_TIMEOUT = 500;
+    private WebDriverWait engagementTimeout;
+
     public WaitUtils(WebDriver driver) {
         this.driver = driver;
     }
 
-//    public void waitVisibilityOfElement(WebElement element, int timeOut) {
+    //    public void waitVisibilityOfElement(WebElement element, int timeOut) {
 //        WebDriverWait webDriverWait = new WebDriverWait(getDriver("chrome"), timeOut);
 //        webDriverWait.until(ExpectedConditions.visibilityOf(element));
 //    }
@@ -149,6 +163,60 @@ public class WaitUtils extends DriverFactory {
 //        waitElementToBeClickableShort(element);
 //        getDriver("chrome").findElement(element).click();
 //    }
+
+    public boolean isElementVisible(WebElement element, int timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        try {
+            return wait.until(WOExpectedConditions.visibilityOf(element)) != null;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean isElementVisible(By locator, int timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        try {
+            return wait.until(WOExpectedConditions.visibilityOf(locator));
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean isElementVisibleNow(By locator) {
+        return isElementVisible(locator, 0);
+    }
+
+    public boolean isElementVisibleAfterShortWait(By locator) {
+        return isElementVisible(locator, SHORT_TIMEOUT);
+    }
+
+    public boolean isElementVisibleAfterMiddleWait(By locator) {
+        return isElementVisible(locator, MIDDLE_TIMEOUT);
+    }
+
+    public boolean isElementVisibleAfterLongWait(By locator) {
+        return isElementVisible(locator, LONG_TIMEOUT);
+    }
+
+    public boolean isElementVisible(WebElement element) {
+        return isElementVisible(element, SHORT_TIMEOUT);
+    }
+
+    public boolean isElementVisibleNow(WebElement element) {
+        return isElementVisible(element, 0);
+    }
+
+    public boolean isElementVisibleAfterShortWait(WebElement element) {
+        return isElementVisible(element, SHORT_TIMEOUT);
+    }
+
+    public boolean isElementVisibleAfterMiddleWait(WebElement element) {
+        return isElementVisible(element, MIDDLE_TIMEOUT);
+    }
+
+    public boolean isElementVisibleAfterLongWait(WebElement element) {
+        return isElementVisible(element, LONG_TIMEOUT);
+    }
 
     public void waitVisibilityOfElement(WebElement element, int timeOut) {
         WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
