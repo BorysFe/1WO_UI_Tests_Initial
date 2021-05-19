fc96ccf88159bc6392bc16f69a75704573981ac2
BorysFe <fesenko@icloud.com>
2021-05-19 20:48:58 +0300

    New poll test- changes

--- 1WO_UI_Tests-master/src/test/java/portal/partners/polls/CreateNewPollsTest.java
+++ 1WO_UI_Tests-master/src/test/java/portal/partners/polls/CreateNewPollsTest.java
@@ -5,12 +5,12 @@ import org.assertj.core.api.Assertions;
 import org.testng.annotations.*;
 import portalPages.FeedPage;
 import portalPages.MenuProfileDropDown;
+import portalPages.polls.RegionsAndLanguages;
+import portalPages.polls.polls.NewPollManagerPage;
+import portalPages.polls.polls.PollCategory;
+import portalPages.polls.polls.PollsPage;
 import portalPages.publisher.OnboardingPublisherPage;
 import portalPages.publisher.PublisherLoginPage;
-import portalPages.publisher.polls.NewPollManagerPage;
-import portalPages.publisher.polls.PollCategory;
-import portalPages.publisher.polls.PollRegionsAndLanguage;
-import portalPages.publisher.polls.PollsPage;
 import utils.DriverFactory;
 
 public class CreateNewPollsTest extends DriverFactory {
@@ -40,7 +40,6 @@ public class CreateNewPollsTest extends DriverFactory {
         onboardingPublisherPage = new OnboardingPublisherPage(driver);
         newPollManager = new NewPollManagerPage(driver);
 
-
         publisherLoginPage.getPublisherLoginPage();
     }
 
@@ -64,11 +63,11 @@ public class CreateNewPollsTest extends DriverFactory {
         publisherLoginPage.loginPublisher(loginPublisher, passwordPublisher);
         onboardingPublisherPage.openPollsPage();
         pollsPage.startNewPollCreating()
-                .selectDropdownAndLanguage(PollRegionsAndLanguage.SELECT_REGION.toString(), PollRegionsAndLanguage.REGION_ALL.toString())
-                .selectDropdownAndLanguage(PollRegionsAndLanguage.SELECT_LANGUAGE.toString(), PollRegionsAndLanguage.LANGUAGE_ENGLISH.toString())
+                .selectDropdownAndItem(RegionsAndLanguages.SELECT_POLL_REGION.toString(), RegionsAndLanguages.REGION_ALL.toString())
+                .selectDropdownAndItem(RegionsAndLanguages.SELECT_POLL_LANGUAGE.toString(), RegionsAndLanguages.LANGUAGE_ENGLISH.toString())
                 .modalSubmit();
 
-        newPollManager.selectPollCategory(PollCategory.CATEGORY_ART_CULTURE.toString())
+        newPollManager.selectPollCategoryOpen(PollCategory.CATEGORY_ART_CULTURE.toString())
                 .fillQuestion(questionText)
                 .fillAnswer("0", String.format(answerText, "1"))
                 .fillAnswer("1", String.format(answerText, "2"))
