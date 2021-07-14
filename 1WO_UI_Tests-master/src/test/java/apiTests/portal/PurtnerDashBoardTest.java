0c3eceb5baa7300ee82f1d46ad1e8f719508a335
1wo_Mac <borys@1worldonline.com>
2021-07-14 17:32:47 +0300

    API first test

---
+++ 1WO_UI_Tests-master/src/test/java/apiTests/portal/PurtnerDashBoardTest.java
@@ -0,0 +1,33 @@
+package apiTests.portal;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+public class PurtnerDashBoardTest {
+    @Test
+    public static void main2() {
+        JSONObject requestBody = new JSONObject();
+
+//        requestBody.put("partners", "f9f5cc9e-cf6c-4c6e-90a1-a54f0863dcbc");
+        requestBody.get("{\n    \"partners\": [\n \"f9f5cc9e-cf6c-4c6e-90a1-a54f0863dcbc\"\n ]\n  }");
+
+        RequestSpecification request = RestAssured.given()
+                .header("cookie", "RememberMe=Ym9yeXNib3J5c0BtYWlsaW5hdG9yLmNvbQ~~#l7DMAoTz70MSh4CXoIp3hA~~")
+                .and()
+                .header("Content-Type", "application/json");
+
+        request.body(requestBody);
+
+        Response response = request.get("https://dev-web-images.s3.amazonaws.com/partners/features-analytics");
+
+        System.out.println("request - " + request);
+        System.out.println("requestBody - " + requestBody);
+
+        System.out.println("statusCode - " + response.getStatusCode());
+        System.out.println("responseBody - " + response.getBody());
+    }
+}
